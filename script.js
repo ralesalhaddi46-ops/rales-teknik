dc4027ade61870ef44a974973efe581b42857038
ralesalhaddi46-ops <ralesalhaddi46@gmail.com>
2026-07-14 21:14:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let topBtn = document.getElementById("topBtn");
 
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach(sec => {
+    sec.classList.add("fade");
+    observer.observe(sec);
+});
